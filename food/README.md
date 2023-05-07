70bdf36ba0986f73ae43413bb4a209b13bd48927
HU JIAJUN <hujiajun19961024@gmail.com>
2023-05-07 21:39:37 +0800

    Add food

---
+++ food/README.md
@@ -0,0 +1,7 @@
+# Food Segmentation
+
+| Model | Type |
+| :----: | :----: |
+| [sam_vit_h_4b8939.pth](https://drive.google.com/file/d/1kJ4xaiVVTpUXEeF9phvkhilxsbpToHW4/view?usp=share_link) | vit_h |
+| [sam_vit_l_0b3195.pth](https://drive.google.com/file/d/1VBVipk1izHDOHH7zWKMHGhzfQv3FeP0v/view?usp=share_link) | vit_l |
+| [sam_vit_b_01ec64.pth](https://drive.google.com/file/d/12seklpq0KeMd3uWCTiGeODBrrqy5bod1/view?usp=share_link) | vit_b |
---
+++ food/dataset.py
@@ -0,0 +1,100 @@
+# https://github.com/facebookresearch/segment-anything/blob/main/segment_anything/automatic_mask_generator.py
+
+import os
+import cv2
+import glob
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from segment_anything import sam_model_registry
+from segment_anything import SamAutomaticMaskGenerator
+
+
+CHECKPOINTS = ["models/sam_vit_h_4b8939.pth", "models/sam_vit_l_0b3195.pth", "models/sam_vit_b_01ec64.pth"]
+MODEL_TYPES = ["vit_h", "vit_l", "vit_b"]
+MODEL = 0
+MIN_MASK_REGION_AREA = 50 * 50
+IMAGE_SIZE = 512
+
+
+RAW_DATA = glob.glob("FoodSeg103/*")
+RAW_DATA_PATH = "FoodSeg103/"
+DATASET_PATH = "dataset/"
+DEVICE = "cuda"
+
+
+def create_dataset():
+    sam = sam_model_registry[MODEL_TYPES[MODEL]](checkpoint=CHECKPOINTS[MODEL])
+    if torch.cuda.is_available():
+        sam.to(device=DEVICE)
+    mask_generator = SamAutomaticMaskGenerator(model=sam)
+    os.makedirs(DATASET_PATH)
+    with tqdm(total=len(RAW_DATA)) as pbar:
+        for i, image in enumerate(RAW_DATA):
+            pbar.set_description("Segmenting: %d" % (1 + i))
+            pbar.update(1)
+            image_name = os.path.splitext(os.path.basename(image))[0]
+            image_dir = os.path.join(DATASET_PATH, image_name)
+            os.makedirs(image_dir)
+            image = cv2.imread(image)
+            image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            masks = mask_generator.generate(image)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            masks = [item for item in masks if item["area"] >= MIN_MASK_REGION_AREA]
+            sorted_annotations = sorted(masks, key=(lambda item: item["area"]), reverse=True)
+            for j, annotation in enumerate(sorted_annotations):
+                img = np.zeros((sorted_annotations[0]["segmentation"].shape[0],
+                                sorted_annotations[0]["segmentation"].shape[1]))
+                m = annotation["segmentation"]
+                img[m] = 1
+                mask = cv2.bitwise_and(image, image, mask=img.astype("uint8"))
+                tmp = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+                _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+                b, g, r = cv2.split(mask)
+                rgba = [b, g, r, alpha]
+                masked_img = cv2.merge(rgba, 4)
+                contours, _ = cv2.findContours(cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY),
+                                               cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                x, y, w, h = cv2.boundingRect(contours[0])
+                cropped_img = masked_img[y:y + h, x:x + w]
+                cv2.imwrite(os.path.join(DATASET_PATH, image_name + ".jpg"), image)
+                cv2.imwrite(os.path.join(image_dir, str(j) + ".jpg"), cropped_img)
+
+
+def plot_image(image, show_mask=False):
+    image = cv2.imread(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.imshow(image)
+    if show_mask:
+        sam = sam_model_registry[MODEL_TYPES[MODEL]](checkpoint=CHECKPOINTS[MODEL])
+        if torch.cuda.is_available():
+            sam.to(device=DEVICE)
+        mask_generator = SamAutomaticMaskGenerator(model=sam)
+        masks = mask_generator.generate(image)
+        show_annotations(masks)
+    plt.axis("off")
+    plt.show()
+
+
+def show_annotations(annotations):
+    if len(annotations) == 0:
+        return
+    sorted_annotations = sorted(annotations, key=(lambda item: item["area"]), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    img = np.ones((sorted_annotations[0]["segmentation"].shape[0], sorted_annotations[0]["segmentation"].shape[1], 4))
+    img[:, :, 3] = 0
+    for annotation in sorted_annotations:
+        m = annotation["segmentation"]
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+
+if __name__ == "__main__":
+    # plot_image(RAW_DATA_PATH + "00000000.jpg", show_mask=True)
+    if not os.path.isdir(DATASET_PATH):
+        create_dataset()
